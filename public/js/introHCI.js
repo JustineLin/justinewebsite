755f132bd32a6c8e88dc77ae1baa418d28e54a7a
Justine Lin <justineelin@gmail.com>
2016-12-14 18:00:17 -0800

    trying to start website

---
+++ public/js/introHCI.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
